2c4bacffb6dda33aaf84f64de715350ecc53e1ca
iam-shrey <shreytripti26@gmail.com>
2024-09-22 23:12:48 +0530

    Initial commit

---
+++ src/main/java/com/telusko/quizapp/controller/QuestionController.java
@@ -0,0 +1,33 @@
+package com.telusko.quizapp.controller;
+
+import com.telusko.quizapp.Question;
+import com.telusko.quizapp.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("question") //it sets a base URL
+public class QuestionController {
+
+    @Autowired
+    QuestionService questionService;
+
+    // Endpoint is available at /question/allQuestions
+    @GetMapping("allQuestions")
+    public List<Question> getAllQuestions(){
+        return questionService.getAllQuestions();
+    }
+
+    @GetMapping("category/{category}")
+    public List<Question> getQuestionsByCategory(@PathVariable String category){
+        return questionService.getQuestionsByCategory(category);
+    }
+
+    @PostMapping("add")
+    public String addQuestion(@RequestBody Question question){
+        return questionService.addQuestion(question);
+    }
+
+}
