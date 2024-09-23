cd98485ce20d8bc842daf06ba6032bdd787fcb90
iam-shrey <shreytripti26@gmail.com>
2024-09-23 19:46:59 +0530

    quiz service and controller made

--- src/main/java/com/telusko/quizapp/model/Question.java
+++ src/main/java/com/telusko/quizapp/model/Question.java
@@ -1,11 +1,10 @@
-package com.telusko.quizapp;
+package com.telusko.quizapp.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
-import org.springframework.stereotype.Repository;
 
 @Data
 @Entity
