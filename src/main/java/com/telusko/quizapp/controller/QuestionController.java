fa7e59345dca4bb1938ca7b4fcc6506e7a1188e4
iam-shrey <shreytripti26@gmail.com>
2024-09-23 11:35:13 +0530

    added Response Entity, response status and exception handling

--- src/main/java/com/telusko/quizapp/controller/QuestionController.java
+++ src/main/java/com/telusko/quizapp/controller/QuestionController.java
@@ -3,6 +3,7 @@ package com.telusko.quizapp.controller;
 import com.telusko.quizapp.Question;
 import com.telusko.quizapp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,17 +17,17 @@ public class QuestionController {
 
     // Endpoint is available at /question/allQuestions
     @GetMapping("allQuestions")
-    public List<Question> getAllQuestions(){
+    public ResponseEntity<List<Question>> getAllQuestions(){
         return questionService.getAllQuestions();
     }
 
     @GetMapping("category/{category}")
-    public List<Question> getQuestionsByCategory(@PathVariable String category){
+    public ResponseEntity<List<Question>> getQuestionsByCategory(@PathVariable String category){
         return questionService.getQuestionsByCategory(category);
     }
 
     @PostMapping("add")
-    public String addQuestion(@RequestBody Question question){
+    public ResponseEntity<String> addQuestion(@RequestBody Question question){
         return questionService.addQuestion(question);
     }
 
