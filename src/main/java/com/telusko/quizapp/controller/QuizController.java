5a9170086bcfc05ffe8bc0c8f83055ee5440b7e1
iam-shrey <shreytripti26@gmail.com>
2024-09-23 23:07:08 +0530

    made get & submit quiz controller, create quiz & calculate result quiz service and questionWrapper & response model

--- src/main/java/com/telusko/quizapp/controller/QuizController.java
+++ src/main/java/com/telusko/quizapp/controller/QuizController.java
@@ -1,12 +1,14 @@
 package com.telusko.quizapp.controller;
 
+import com.telusko.quizapp.model.Question;
+import com.telusko.quizapp.model.QuestionWrapper;
+import com.telusko.quizapp.model.Response;
 import com.telusko.quizapp.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("quiz")
@@ -18,6 +20,16 @@ public class QuizController {
     @PostMapping("create")
     public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam int numQ, @RequestParam String title){
         return quizService.createQuiz(category, numQ, title);
+    }
+
+    @GetMapping("get/{id}")
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable Integer id){
+        return quizService.getQuizQuestions(id);
+    }
+
+    @PostMapping("submit/{id}")
+    public ResponseEntity<Integer> submitAnswer(@PathVariable Integer id,@RequestBody List<Response> responses){
+        return quizService.calculateResult(id, responses);
     }
 
 }
